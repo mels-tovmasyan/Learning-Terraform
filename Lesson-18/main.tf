658e6f985da3d7b952a98d1e80ba2591728eb954
mels-tovmasyan <tovmasyan.mels@gmail.com>
2022-11-11 10:20:10 +0400

    Provision resources in multiple Regions/Accounts

--- Lesson-18/main.tf
+++ Lesson-18/main.tf
@@ -4,46 +4,38 @@
 
 provider "aws" {
   region = "us-east-1"
-  alias  = "USA"
+  alias  = "us"
 }
-
 
 provider "aws" {
   region = "ca-central-1"
-  alias  = "CA"
+  alias  = "ca"
 }
-
 
 provider "aws" {
   region = "eu-central-1"
 }
 
+//asume role to a different account
+# asume_role {
+#   role_arn = "arn:aws:iam::01234567890:role/role_in_account_b"
+# }
 
 resource "aws_instance" "my_us_server" {
-  provider      = aws.USA
+  provider      = aws.us
   ami           = "ami-09d3b3274b6c5d4aa"
   instance_type = "t2.micro"
   tags = {
-    Name = "USA Server"
-  }
-}
-
-
-data "aws_ami" "ca_lates_amzn_linux" {
-  owners      = ["amazon"]
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-*"]
+    Name = "us Server"
   }
 }
 
 resource "aws_instance" "my_ca_server" {
-  provider      = aws.CA
-  ami           = data.aws_ami.ca_lates_amzn_linux.id
+  provider      = aws.ca
+  ami           = data.aws_ami.ca_latest_amzn_linux.id
   instance_type = "t2.micro"
   tags = {
-    Name = "Canada Server"
+    Name = "canada Server"
   }
 }
 
@@ -55,8 +47,45 @@ resource "aws_instance" "my_default_server" {
   }
 }
 
+# ----------------------------------------------------------
+data "aws_ami" "us_latest_amzn_linux" {
+  provider    = aws.us
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-*"]
+  }
+}
 
+data "aws_ami" "ca_latest_amzn_linux" {
+  provider    = aws.ca
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-*"]
+  }
+}
 
-output "latest_linux_ami_id" {
-  value = data.aws_ami.ca_lates_amzn_linux.id
+data "aws_ami" "def_latest_amzn_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-*"]
+  }
+}
+# ----------------------------------------------------------
+
+output "us_linux_ami_id" {
+  value = data.aws_ami.us_latest_amzn_linux.id
+}
+
+output "ca_linux_ami_id" {
+  value = data.aws_ami.ca_latest_amzn_linux.id
+}
+
+output "def_linux_ami_id" {
+  value = data.aws_ami.def_latest_amzn_linux.id
 }
