3c38972538514fab1e90c8a3c7136ca2858972ed
mels-tovmasyan <tovmasyan.mels@gmail.com>
2022-11-15 17:10:08 +0400

    Import existing resources with terraform import

--- Lesson-22/import.tf
+++ Lesson-22/import.tf
@@ -5,7 +5,7 @@
 #   1 Find instance_id e.g i-0b84bc5f76fc2a8f0
 #   2 Create blank resource e.g "aws_instance" "my_ec2"
 #   3 Run terraform init command
-#   4 Run terraform inport aws_instance.my_ec2 i-0b84bc5f76fc2a8f0
+#   4 Run terraform import aws_instance.my_ec2 i-0b84bc5f76fc2a8f0
 #   5 Run terraform apply
 #   6 Fill in missing required fields
 #   7 Run terraform apply again
